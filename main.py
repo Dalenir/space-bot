4da1cd193a0e3878381a8839c17f5026925bf6d1
Neveric <kerkker@gmail.com>
2022-08-14 01:36:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 
 async def main():
-    bot = await SpaceBot.rise(os.environ.get('INPUT_ORGANISATION_URL'), os.environ.get('INPUT_APP_SECRET'),
+    bot = await SpaceBot.rise(os.environ.get('INPUT_ORGANISATION_URL'), os.environ.get('INPUT_APP_ID'),
                               os.environ.get('INPUT_APP_SECRET'))
     bot.send_message(os.environ.get('INPUT_CHAT_TITLE'), os.environ.get('INPUT_MESSAGE'))
     print("::set-output name=result::Done!")
