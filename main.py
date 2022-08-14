8c4d300a045fca43669bc767c359b07aaed4f838
Neveric <60537405+Dalenir@users.noreply.github.com>
2022-08-14 20:29:59 +0300

    7

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import re
 
 from spacebot import SpaceBot
 import os
@@ -11,13 +12,23 @@ async def main():
                                   os.environ.get('INPUT_APP_SECRET'), os.environ.get('INPUT_PROJECT'))
         if os.environ.get('INPUT_MESSAGE'):
             await bot.send_message(os.environ.get('INPUT_CHAT_TITLE'), os.environ.get('INPUT_MESSAGE'))
-        if os.environ.get('INPUT_ISSUE_NUMBERS') and os.environ.get('INPUT_TAG') and os.environ.get('INPUT_PROJECT'):
-            for iss in os.environ.get('INPUT_ISSUE_NUMBERS').replace("'", '').split(' '):
-                print(iss)
-                try:
-                    asyncio.create_task(bot.update_issue_tag(int(iss), os.environ.get('INPUT_TAG')))
-                except Exception as ex:
-                    logging.error(ex)
+        issue_numbers, commit_titles = os.environ.get('INPUT_ISSUE_NUMBERS'), os.environ.get('INPUT_COMMITS_TITLES')
+        if (issue_numbers or commit_titles) and os.environ.get('INPUT_TAG') and os.environ.get('INPUT_PROJECT'):
+            iss_list = list()
+            if issue_numbers:
+                iss_list += [iss for iss in issue_numbers.split(" ")]
+            if commit_titles:
+                iss_list += [re.sub("[^0-9]", "", comm) for
+                             comm in [b for c in [a.split(" ") for
+                                                  a in commit_titles.split("||")] for
+                                      b in c if b] if re.sub("[^0-9]", "", comm)]
+            for iss in os.environ.get('INPUT_ISSUE_NUMBERS').split("||"):
+                iss = iss.replace(" ")
+                if iss.isdigit():
+                    try:
+                        asyncio.create_task(bot.update_issue_tag(int(iss), os.environ.get('INPUT_TAG')))
+                    except Exception as ex:
+                        logging.error(ex)
         await asyncio.gather(*asyncio.all_tasks() - {asyncio.current_task()})
         print("::set-output name=result::Done!")
     except Exception as ex:
