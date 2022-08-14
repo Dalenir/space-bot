8f8e0d155d46723c70907597afd0fb6ef53d7a8b
Neveric <60537405+Dalenir@users.noreply.github.com>
2022-08-14 17:17:18 +0300

    Tags

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ import os
 async def main():
     try:
         bot = await SpaceBot.rise(os.environ.get('INPUT_ORGANISATION_URL'), os.environ.get('INPUT_APP_ID'),
-                                  os.environ.get('INPUT_APP_SECRET'))
-        await bot.send_message(os.environ.get('INPUT_CHAT_TITLE'), os.environ.get('INPUT_MESSAGE'))
+                                  os.environ.get('INPUT_APP_SECRET'), os.environ.get('INPUT_PROJECT'))
+        if os.environ.get('INPUT_MESSAGE'):
+            await bot.send_message(os.environ.get('INPUT_CHAT_TITLE'), os.environ.get('INPUT_MESSAGE'))
+        if os.environ.get('INPUT_ISSUE_NUMBERS') and os.environ.get('INPUT_TAG') and os.environ.get('INPUT_PROJECT'):
+            for iss in os.environ.get('INPUT_ISSUE_NUMBERS').replace("'", '').split(' '):
+                print(iss)
+                try:
+                    asyncio.create_task(bot.update_issue_tag(int(iss), os.environ.get('INPUT_TAG')))
+                except Exception as ex:
+                    logging.error(ex)
+        await asyncio.gather(*asyncio.all_tasks() - {asyncio.current_task()})
         print("::set-output name=result::Done!")
+        asyncio.lo
     except Exception as ex:
         logging.error(ex)
 
